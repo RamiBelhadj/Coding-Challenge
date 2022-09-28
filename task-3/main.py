005cbbbfdea3d9ffb52529d3299b42365530f8f8
RamiBelhadj <belhadjmohamedrami@gmail.com>
2022-09-28 10:41:42 +0200

    Task3 with O(N) time exexution

--- task-3/main.py
+++ task-3/main.py
@@ -5,30 +5,56 @@ def get_data_from_db():
     with open('input-task-3.json') as json_file:
         return json.load(json_file)
 class Node: 
-    def __init__(self, title, id, children):
+    def __init__(self, title, id):
         self.title = title
         self.id = id
-        self.children = children
+        self.children = []
+    def add(self, child):
+        self.children.append(child)
+        
+def List(tree, head):
+    if tree.get(head):
+        for node in tree[head]: 
+            print(node.id)
+            List(tree,node.id)
 
 class Tree: 
     def __init__(self): 
         data = get_data_from_db()
-        
-        for node in data : 
+        tree = dict()
+        for node in data: 
             if not node["parent_knowledge_id"]: 
-                self.head = Node(node["title"], node["id"], [])
-        depth = 0
-        print(self.head)
-        x = self.head
-        for node in data : 
-            if node["parent_knowledge_id"] == self.head.id: 
-                print('asd')
-                print(self.head)
-                x.children.append(Node(node["title"], node["id"], []))
+                tree["head"] = []
+                tree["head"].append(Node(node["title"],node["id"]))
             else: 
-                print(depth)
-                depth = depth +1 
-                x = x.children
+                if not tree.get(node["parent_knowledge_id"]): 
+                    tree[node["parent_knowledge_id"]] = []
+                    tree[node["parent_knowledge_id"]].append(Node(node["title"],node["id"]))
+                else: 
+                    tree[node["parent_knowledge_id"]].append(Node(node["title"],node["id"]))
+        List(tree, "head")
+        # for node in data : 
+        #     if not node["parent_knowledge_id"]: 
+        #         self.head = Node(node["title"], node["id"])
+        #         depth = 0
+        #         x = []
+        #         x.append(self.head)
+        #     else:
+        #         found = False
+        #         for y in x:
+                    
+        #             if node["parent_knowledge_id"] == y.id:
+        #                 print('found') 
+        #                 y.add(Node(node["title"], node["id"]))
+        #                 found = True
+        #                 break
+        #             if not found: 
+        #                 x1 = []    
+        #                 for y in x: 
+                            
+        #                     x1.append(y.children)
+        #                 x = x1
+       
         pass 
 
 if __name__ == '__main__':
